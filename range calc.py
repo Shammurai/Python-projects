b2f10b3b39e31ac44a710f93fc5e852821f8b056
Shammurai <Jacobstephens1@live.com>
2024-10-10 22:36:48 -0400

    range calc

---
+++ range calc.py
@@ -0,0 +1,13 @@
+ for numbers in  range(1, 100):
+
+if numbers % == 3:
+    print("Fizz")
+    
+elif numbers % == 5:
+    print("Buzz")
+    
+else numbers % == 5 or 3:
+    print("FizzBuzz")
+    
+    else:
+    print(number)
